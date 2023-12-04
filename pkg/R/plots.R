d8b9267ffb69a4a3b20e0d64629b75399d59aa5b
kemery (Kevin Emery) <kekemery22@gmail.com>
2023-12-04 07:50:51 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqimpute@118 94703add-9fa6-41e5-96f8-9d1048fa918d

--- pkg/R/plots.R
+++ pkg/R/plots.R
@@ -25,7 +25,15 @@ seqwithmiss <- function(seqdata){
   if(!inherits(seqdata,"stslist")){
     stop("seqdata is not a sequence object.")
   }
-  rowsNA <- rowSums(seqdata==attr(seqdata,"nr"))
+  if(is.na(attr(seqdata,"nr"))){
+    tmp <- seqdata
+    for(i in 1:ncol(seqdata)){
+      tmp[,i] <- as.character(seqdata[,i])
+    }
+    rowsNA <- rowSums(is.na(tmp))
+  }else{
+    rowsNA <- rowSums(seqdata==attr(seqdata,"nr"))
+  }
   return(seqdata[rowsNA!=0,])
 }
 
@@ -59,15 +67,29 @@ seqmissfplot <- function(seqdata,...){
 #' @export
 seqmissIplot <- function(seqdata,...){
   seqmiss <- seqwithmiss(seqdata)
+  if(nrow(seqmiss)==0){
+    stop("The provided sequence object has no missing values. Note that the 
+            default behavior of the seqdef() function is to delete missing values 
+            at the end of the sequences. If it is not what you want, please consider
+            modifying it.")
+    
+  }
   misspatterns <- matrix(NA,nrow(seqmiss),ncol(seqmiss))
   misspatterns <- as.data.frame(misspatterns)
   colnames(misspatterns) <- colnames(seqmiss)
-  
-  misspatterns[seqmiss==attr(seqmiss,"nr")] <- "missing"
-  misspatterns[seqmiss!=attr(seqmiss,"nr")] <- "observed"
-  seqtest <- suppressMessages(TraMineR::seqdef(misspatterns,alphabet = c("observed","missing"),cpal=c("blue","red"),xtstep=attr(seqmiss,"xtstep")))
-  
-  TraMineR::seqIplot(seqtest,...)
+  if(!is.na(attr(seqmiss,"nr"))){
+    misspatterns[seqmiss==attr(seqmiss,"nr")] <- "missing"
+    misspatterns[seqmiss!=attr(seqmiss,"nr")] <- "not missing"
+    seqtest <- suppressMessages(TraMineR::seqdef(misspatterns,alphabet = c("not missing","missing"),cpal=c("blue","red"),xtstep=attr(seqmiss,"xtstep")))
+    
+    TraMineR::seqIplot(seqtest,...)
+  }else{
+    misspatterns[is.na(seqmiss)] <- "missing"
+    misspatterns[!is.na(seqmiss)] <- "not missing"
+    seqtest <- suppressMessages(TraMineR::seqdef(misspatterns,alphabet = c("not missing","missing"),cpal=c("blue","red"),xtstep=attr(seqmiss,"xtstep")))
+    
+    TraMineR::seqIplot(seqtest,...)
+  }
   
 }
 
@@ -85,4 +107,27 @@ seqmissimplic <- function(seqdata,...){
   tt[rowSums(seqdata==attr(seqdata,"nr"))==0] <- "observed"
   imp <- TraMineRextras::seqimplic(seqdata,tt)
   return(imp) 
+}
+
+
+#' Function that adds the clustering result to an imputed dataset
+#' obtained with seqimpute
+#' 
+#' @param imputed a dataframe obtained with the seqimpute function
+#' @param clustering clustering made on the stacked on multiple imputed datasets
+#' 
+#' @export
+addcluster <- function(imputed, clustering){
+  imputed$cluster <- NA
+  if(!".imp"%in%colnames(imputed)){
+    error("The provided dataset does not have any columns .imp")
+  }
+  if(!0%in%imputed$.imp){
+    imputed$cluster <- clustering
+  }else{
+    tt <- which(rowSums(is.na(imputed[imputed$.imp==0,]))==0)
+    imputed[tt,] <- clustering[tt]
+    imputed[imputed$.imp!=0,"cluster"] <- clustering
+  }
+  return(imputed)
 }
