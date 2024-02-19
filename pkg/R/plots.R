818e0471c975270df7825b581f0e3123c2b0a2a8
kemery (Kevin Emery) <kekemery22@gmail.com>
2024-02-19 09:12:59 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqimpute@127 94703add-9fa6-41e5-96f8-9d1048fa918d

--- pkg/R/plots.R
+++ pkg/R/plots.R
@@ -1,3 +1,21 @@
+#' Extract only the completed datasets from the results obtained with seqimpute function. 
+#' Therefore, the original dataset with missing values is discarded,
+#' together with the two first columns (".id" and ".imp")".
+#' 
+#' 
+#' @param impdata a dataframe obtained with the seqimpute() function
+#' @return Returns a data frame that has the form for the application of
+#' Halpin's clustering strategy
+#' 
+#' @export
+onlyimputed <- function(impdata){
+  if(".imp"%in%colnames(impdata)&".id"%in%colnames(impdata)){
+    to_remove <- c(".imp", ".id")
+    impdata <- impdata[ impdata$.imp!=0, !(names(impdata) %in% to_remove)]
+    return(impdata)
+  }
+  
+}
 
 #' Extract all the sequences without missing value.
 #' 
@@ -17,8 +35,8 @@ seqcomplete <- function(seqdata){
 #' Extract all the sequences with at least one missing value
 #' 
 #' @param seqdata a state sequence object built with the TraMineR package
-#' @return Returns either a data frame or a state sequence object, depending
-#' the type of data that was provided to the function
+#' @return Returns a state sequence object containing only
+#' the sequences with at least one missing value
 #' 
 #' @export
 seqwithmiss <- function(seqdata){
@@ -41,6 +59,8 @@ seqwithmiss <- function(seqdata){
 #' Plot of the most common patterns of missing data.
 #' 
 #' @param seqdata a state sequence object built with the TraMineR package
+#' @param with.complete a logical stating if complete trajectories should be included or not in the plot
+#'
 #' @param ... parameters to be passed to the seqfplot function 
 #' 
 #' @export
@@ -66,6 +86,7 @@ seqmissfplot <- function(seqdata, with.complete=TRUE,...){
 #' Plot all the patterns of missing data.
 #' 
 #' @param seqdata a state sequence object built with the TraMineR package
+#' @param with.complete a logical stating if complete trajectories should be included or not in the plot
 #' @param ... parameters to be passed to the seqIplot function 
 #' 
 #' @export
@@ -76,10 +97,7 @@ seqmissIplot <- function(seqdata, with.complete=TRUE,...){
     seqmiss <- seqwithmiss(seqdata)
   }
   if(nrow(seqmiss)==0){
-    stop("The provided sequence object has no missing values. Note that the 
-            default behavior of the seqdef() function is to delete missing values 
-            at the end of the sequences. If it is not what you want, please consider
-            modifying it.")
+    stop("The provided sequence object has no missing values.")
     
   }
   misspatterns <- matrix(NA,nrow(seqmiss),ncol(seqmiss))
@@ -87,14 +105,14 @@ seqmissIplot <- function(seqdata, with.complete=TRUE,...){
   colnames(misspatterns) <- colnames(seqmiss)
   if(!is.na(attr(seqmiss,"nr"))){
     misspatterns[seqmiss==attr(seqmiss,"nr")] <- "missing"
-    misspatterns[seqmiss!=attr(seqmiss,"nr")] <- "not missing"
-    seqtest <- suppressMessages(TraMineR::seqdef(misspatterns,alphabet = c("not missing","missing"),cpal=c("blue","red"),xtstep=attr(seqmiss,"xtstep")))
+    misspatterns[seqmiss!=attr(seqmiss,"nr")] <- "observed"
+    seqtest <- suppressMessages(TraMineR::seqdef(misspatterns,alphabet = c("observed","missing"),cpal=c("blue","red"),xtstep=attr(seqmiss,"xtstep")))
     
     TraMineR::seqIplot(seqtest,...)
   }else{
     misspatterns[is.na(seqmiss)] <- "missing"
-    misspatterns[!is.na(seqmiss)] <- "not missing"
-    seqtest <- suppressMessages(TraMineR::seqdef(misspatterns,alphabet = c("not missing","missing"),cpal=c("blue","red"),xtstep=attr(seqmiss,"xtstep")))
+    misspatterns[!is.na(seqmiss)] <- "observed"
+    seqtest <- suppressMessages(TraMineR::seqdef(misspatterns,alphabet = c("observed","missing"),cpal=c("blue","red"),xtstep=attr(seqmiss,"xtstep")))
     
     TraMineR::seqIplot(seqtest,...)
   }
@@ -107,7 +125,7 @@ seqmissIplot <- function(seqdata, with.complete=TRUE,...){
 #' See the seqimplic helps to more details on how it works.
 #' 
 #' @param seqdata a state sequence object built with the TraMineR package
-#' @param ... parameters to be passed to the seqIplot function 
+#' @param ... parameters to be passed to the seqimplic() function 
 #' 
 #' @export
 seqmissimplic <- function(seqdata,...){
